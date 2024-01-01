5bdad56299facd1f6e820f35fc9dee9ac7f79aea
VasilyeuUV <vasilyeu.uv@mail.ru>
2024-01-01 18:39:42 +0300

    feat: solve generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -380,8 +380,10 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  if (len === 0) return [];
+  const length = len;
+  return Array.from({ length }).map((v, i) => i * 2 + 1);
 }
 
 /**

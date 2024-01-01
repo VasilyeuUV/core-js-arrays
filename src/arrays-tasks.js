f2f8934528561f7e45b007080810c0d0a58a4066
VasilyeuUV <vasilyeu.uv@mail.ru>
2024-01-01 19:54:45 +0300

    feat: solve getElementByIndices

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -381,7 +381,7 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
 function generateOdds(len) {
-  if (len === 0) return [];
+  if (len < 1) return [];
   const length = len;
   return Array.from({ length }).map((v, i) => i * 2 + 1);
 }
@@ -398,8 +398,10 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  return indices.length > 1
+    ? getElementByIndices(arr[indices.shift()], indices)
+    : arr[indices.shift()];
 }
 
 /**
